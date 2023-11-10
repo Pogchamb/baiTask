a99883357b908d72edd040b92f1ee9ae5d025a5c
pogch <pogchamb1@gmail.com>
2023-11-10 10:49:09 +0300

    Initial commit

---
+++ app/src/main/java/pa/chan/baitask/features/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package pa.chan.baitask.features.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
